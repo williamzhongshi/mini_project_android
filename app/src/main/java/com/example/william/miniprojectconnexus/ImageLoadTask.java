ebab8bdc9bee6a3c7c1c58970547785b8e59c5bb
Swapna <swapna.mukrappilly@utexas.edu>
2017-10-23 13:38:34 -0500

    Gridview loading URLs

---
+++ app/src/main/java/com/example/william/miniprojectconnexus/ImageLoadTask.java
@@ -0,0 +1,54 @@
+package com.example.william.miniprojectconnexus;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by mukraswa on 10/22/2017.
+ */
+
+public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
+
+    private String url;
+    private ImageView imageView;
+
+    public ImageLoadTask(String url, ImageView imageView) {
+        this.url = url;
+        this.imageView = imageView;
+        Log.e("ImageLoadTask","Inside Constructor");
+    }
+
+    @Override
+    protected Bitmap doInBackground(Void... paramas ) {
+        try {
+            Bitmap myBitmap = null;
+            URL urlConnection = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlConnection
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (Exception e) {
+            //e.printStackTrace();
+            Log.e("ImageLoadTask","Error getting Bitmap");
+            return null;
+        }
+
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        super.onPostExecute(result);
+        imageView.setImageBitmap(result);
+    }
+
+}
