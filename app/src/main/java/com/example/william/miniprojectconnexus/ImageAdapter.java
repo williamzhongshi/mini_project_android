4cd596de1031f07a5b23b0932f3ba3be8f3458e5
Swapna <swapna.mukrappilly@utexas.edu>
2017-10-23 13:38:50 -0500

    ImageAdapter by Swapna

---
+++ app/src/main/java/com/example/william/miniprojectconnexus/ImageAdapter.java
@@ -0,0 +1,85 @@
+package com.example.william.miniprojectconnexus;
+
+/**
+ * Created by mukraswa on 10/20/2017.
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+import org.json.JSONArray;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context context;
+    private List<String> logos;
+    private ImageView imageView;
+    private ImageLoader imageLoader;
+
+
+    public ImageAdapter(Context context, List<String> logos) {
+        this.context = context;
+        this.logos = logos;
+
+        Log.e("ImageAdapter","Inside ImageAdapter");
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return logos.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return logos.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        ImageView imageview;
+        if(view == null){
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+
+        }
+        else {
+        imageView = (ImageView) view;
+        }
+
+        new ImageLoadTask(logos.get(i), imageView).execute();
+
+        return imageView;
+
+    }
+
+    public Context getApplicationContext() {
+        return context;
+    }
+
+
+}
